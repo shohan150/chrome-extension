42663685610510d9e993f03796307e30e3b5a092
shohan <shohan.ece17@gmail.com>
2023-10-02 22:44:19 +0600

    kkk

---
+++ The Code Train/background.js
@@ -0,0 +1,5 @@
+chrome.action.onClicked.addListener(buttonClicked);
+
+function buttonClicked(tab) {
+   console.log('click');
+}
---
+++ The Code Train/content.js
@@ -0,0 +1,6 @@
+console.log('extension working');
+
+let paragraphs = document.getElementsByTagName('p');
+for (elemnt of paragraphs) {
+   elemnt.style['background-color'] = '#ff00ff';
+}
