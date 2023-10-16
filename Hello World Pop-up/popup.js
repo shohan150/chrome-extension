7481af5b52f597d33d469e4c020d0ea280c37340
shohan <shohan.ece17@gmail.com>
2023-10-16 16:41:29 +0600

    popup

---
+++ Hello World Pop-up/popup.js
@@ -0,0 +1,10 @@
+$(function () {
+   //Using jQuery to define a function that will be executed when the DOM is fully loaded and ready. 
+
+   // This is a common pattern in jQuery to ensure that your JavaScript code doesn't run until the HTML document is ready for manipulation. $(function(){ ... }) or $(document).ready(function(){ ... }) waits for the DOM (HTML document) to be fully loaded and ready. Once the DOM is ready, the code inside the function (the ... part) will be executed. This is a good practice to make sure your JavaScript code doesn't run into issues related to trying to interact with HTML elements that haven't been loaded yet.
+
+   $('#name').keyup(function () {
+      $('#greet').text('Hello ' + $('#name').val());
+   })
+
+})
