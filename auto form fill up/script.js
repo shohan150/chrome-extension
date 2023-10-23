52035b9f45517792c1a76bd483842bc6117dfee7
shohan <shohan.ece17@gmail.com>
2023-10-23 15:01:07 +0600

    2nd

--- auto form fill up/script.js
+++ auto form fill up/script.js
@@ -1,4 +1,8 @@
 var abc = document.querySelectorAll('form input[type="text"]');
+
+var rand = ['karim', 'rahim', 'sujon']
 for (var i = 0; i < abc.length; i++) {
-   abc[i].value = 'hidden';
+   var counter = Math.floor(Math.random() * rand.length);
+   abc[i].value = rand[counter];
+   console.log(counter);
 }
