5b018b10ec980371a41849026ba7054920628cf9
shohan <shohan.ece17@gmail.com>
2023-10-23 20:29:01 +0600

    priliminary stage

--- auto form fill up/script.js
+++ auto form fill up/script.js
@@ -1,8 +1,19 @@
 var abc = document.querySelectorAll('form input[type="text"]');
 
-var rand = ['karim', 'rahim', 'sujon']
+var names = ['karim', 'rahim', 'sujon', 'fatema', 'rakib', 'raihan'];
 for (var i = 0; i < abc.length; i++) {
-   var counter = Math.floor(Math.random() * rand.length);
-   abc[i].value = rand[counter];
-   console.log(counter);
+   var counter = Math.floor(Math.random() * names.length);
+   abc[i].value = names[counter];
+}
+
+var email = document.querySelectorAll('form input[type="email"]');
+for (var i = 0; i < email.length; i++) {
+   var counter = Math.floor(Math.random() * names.length);
+   email[i].value = `${names[counter]}@gmail.com`;
+}
+
+var allSelectFields = document.querySelectorAll('form select');
+for (var i = 0; i < allSelectFields.length; i++) {
+   var counter = Math.floor(Math.random() * allSelectFields[i].length);
+   allSelectFields[i].selectedIndex = counter;
 }
