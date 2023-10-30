fc2057f9534c654508f2b044afb6f187fb10ec03
shohan <shohan.ece17@gmail.com>
2023-10-30 22:31:44 +0600

    habe been able to collect the images

--- _Imaged/popup.js
+++ _Imaged/popup.js
@@ -11,14 +11,14 @@ document.getElementById('all').addEventListener('click', function () {
 
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-   console.log(message);
-   console.log(typeof (message));
    const container = document.getElementById("allImg");
    for (let i = 0; i < message.length; i++) {
-      const divs = document.createElement('div');
-      const productImages = document.createElement("img");
-      productImages.src = message[i];
-      container.appendChild(divs);
-      divs.appendChild(productImages);
+      if (message[i]) {
+         const divs = document.createElement('div');
+         const productImages = document.createElement("img");
+         productImages.src = message[i];
+         container.appendChild(divs);
+         divs.appendChild(productImages);
+      }
    }
 });
