828d9cbdf71dae7f5d09e9327cecd2e3e6fe364d
shohan <shohan.ece17@gmail.com>
2023-10-31 20:48:51 +0600

    initially submit

--- _Imaged/content.js
+++ _Imaged/content.js
@@ -6,14 +6,24 @@ for (images of imgs) {
    imgs2.push(images.src);
 }
 
+//product name
+const productName = document.querySelector('.product-details h1').innerText;
+
+//price
+const productPrice = document.querySelector('.product-price span').innerText;
 
 
-
+//description
+const productDes = document.querySelector('#product-tab-description .full-description').innerText;
+console.log(productDes);
 
 //establish connection between the content_script and popup.js
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
    // console.log('Message received in content script:', message.message);
-   chrome.runtime.sendMessage(imgs2);
+   chrome.runtime.sendMessage({ type: "img", data: imgs2 });
+   chrome.runtime.sendMessage({ type: "name", data: productName });
+   chrome.runtime.sendMessage({ type: "price", data: productPrice });
+   chrome.runtime.sendMessage({ type: "des", data: productDes });
 });
 
 
--- _Imaged/popup.js
+++ _Imaged/popup.js
@@ -10,15 +10,46 @@ document.getElementById('all').addEventListener('click', function () {
 
 
 const container = document.getElementById("allImg");
+const info = document.getElementById("product-info");
+
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-   container.innerHTML = '';
-   for (let i = 0; i < message.length; i++) {
-      if (message[i]) {
-         const divs = document.createElement('div');
-         const productImages = document.createElement("img");
-         productImages.src = message[i];
-         container.appendChild(divs);
-         divs.appendChild(productImages);
+   if (message.type === "name") {
+      const productName = document.createElement('h2');
+      productName.innerText = `Product Name: ${message.data}`;
+      info.appendChild(productName);
+   }
+   if (message.type === "price") {
+      const productPrice = document.createElement('h1');
+      productPrice.innerText = message.data;
+      info.appendChild(productPrice);
+   }
+   if (message.type === "des") {
+      const productDes = document.createElement('p');
+      productDes.innerText = message.data;
+      info.appendChild(productDes);
+   }
+   // if (message.type === "img") {
+   //    container.innerHTML = '';
+   //    for (let i = 0; i < message.data.length; i++) {
+   //       if (message.data[i]) {
+   //          const divs = document.createElement('div');
+   //          const productImages = document.createElement("img");
+   //          productImages.src = message.data[i];
+   //          container.appendChild(divs);
+   //          divs.appendChild(productImages);
+   //       }
+   //    }
+   // }
+   if (message.type === "img") {
+      container.innerHTML = '';
+      const divs = document.createElement('div');
+      container.appendChild(divs);
+      for (let i = 0; i < message.data.length; i++) {
+         if (message.data[i]) {
+            const productImages = document.createElement("img");
+            productImages.src = message.data[i];
+            divs.appendChild(productImages);
+         }
       }
    }
 });
