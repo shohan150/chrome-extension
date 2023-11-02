702642e22085561e1749a75e2e44872768f1d824
shohan <shohan.ece17@gmail.com>
2023-11-02 20:50:56 +0600

    learning

--- inject content 2/content.js
+++ inject content 2/content.js
@@ -1,9 +1,18 @@
-const init = function () {
-   const injectElement = document.createElement('div');
-   injectElement.className = 'newElement';
-   injectElement.innerHTML = 'Element successfully added';
-   document.body.appendChild(injectElement);
+if (typeof init === 'undefined') {
+   const init = function () {
+      const injectElement = document.createElement('div');
+      injectElement.className = 'newElement';
+      injectElement.innerHTML = `<style>
+      .newElement {
+         font-size: 24px;
+         color: white;
+         background-color: black;
+         padding: 20px;
+      }
+      </style>
+      Element successfully added again.`;
+      document.body.appendChild(injectElement);
 
+   }
+   init();
 }
-init();
-
