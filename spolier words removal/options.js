746640fe657008238acded26dc0d932fb42a4218
shohan <shohan.ece17@gmail.com>
2023-11-04 21:44:26 +0600

    best but not necessary

--- spolier words removal/options.js
+++ spolier words removal/options.js
@@ -18,27 +18,61 @@ const saveOptions = (saveArray) => {
    );
 };
 
-// Restores select box and checkbox state using the preferences stored in chrome.storage.
+// restore options from the synchronized storage and then calling another function to display those options.
+
+// retrieve data from the synchronized storage. You're specifying that you want to retrieve an object with a property called keywordArray. If this property doesn't exist, it will default to an empty array []. { keywordArray: [] }: This object specifies the default values for the items you want to retrieve. Once the data is retrieved, the provided callback function is executed.
+
+// (items) => { ... }: This is the callback function that receives the retrieved data as the items parameter. In this case, items will be an object with a property keywordArray that holds an array of keywords.
+
+// Inside the callback, you call the buildOptionDisplay function and pass the keywordArray from the retrieved items as an argument. This function is responsible for building the display for the options. The buildOptionDisplay function is defined to handle the rendering of options. It takes the items array as a parameter.
 const restoreOptions = () => {
    chrome.storage.sync.get({ keywordArray: [] }, (items) => {
       buildOptionDisplay(items.keywordArray);
    });
 };
 
-function buildOptionDisplay(items) {
-   for (var i = 0; i < items.length; i++) {
+function buildOptionDisplay(item) {
+   for (var i = 0; i < item.length; i++) {
 
    }
 }
 
-
-//add listener to add keyword button
-document.querySelector('.add-keyword').addEventListener('click', () => {
+function createRowWithOptions(obj) {
    var individualWords = document.querySelector('.individual-words').innerHTML;
    var newRow = document.createElement('div');
    newRow.innerHTML = individualWords;
    newRow.classList.add('individual-words');
+   var time = new Date();
+   var timeStamp = time.toISOString();
+   newRow.dataset.id = timeStamp;
    document.querySelector('.keyword-row').appendChild(newRow);
+
+   var newElement = document.querySelector('.keyword-holder .keyword-row .individual-words[data-id="' + timeStamp + '"]');
+   console.log(newElement);
+
+   newElement.querySelector('.keyword input').value = obj.keyword;
+   newElement.querySelector('.type select').value = obj.type;
+   if (obj.type == '1') {
+      newElement.querySelector('.replace').style.display = 'block';
+      newElement.querySelector('.replace input').value = obj.replace;
+   }
+   newElement.querySelector('.type select').addEventListener('change', (e) => {
+      if (e.target.value == '1') {
+         newElement.querySelector('.replace').style.display = 'block';
+      } else {
+         newElement.querySelector('.replace').style.display = 'none';
+      }
+   })
+}
+
+//add listener to add keyword button
+document.querySelector('.add-keyword').addEventListener('click', () => {
+   var obj = {
+      keyword: 'example',
+      type: '1',
+      replace: 'string'
+   };
+   createRowWithOptions(obj);
 })
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
