bf28a8beb9a35e84766285ddf0f966d61a5db112
shohan <shohan.ece17@gmail.com>
2023-11-04 19:32:16 +0600

    hola

--- spolier words removal/options.js
+++ spolier words removal/options.js
@@ -1,6 +1,12 @@
 // Saves options to chrome.storage
-const saveOptions = () => {
-   chrome.storage.sync.set({},
+
+function buildSaveArray() {
+   var saveArray = [];
+   saveOptions(saveArray);
+}
+
+const saveOptions = (saveArray) => {
+   chrome.storage.sync.set({ keywordArray: saveArray },
       () => {
          // Update status to let user know options were saved.
          const status = document.getElementById('status');
@@ -14,16 +20,26 @@ const saveOptions = () => {
 
 // Restores select box and checkbox state using the preferences stored in chrome.storage.
 const restoreOptions = () => {
-   chrome.storage.sync.get({}, (items) => {
-   }
-   );
+   chrome.storage.sync.get({ keywordArray: [] }, (items) => {
+      buildOptionDisplay(items.keywordArray);
+   });
 };
+
+function buildOptionDisplay(items) {
+   for (var i = 0; i < items.length; i++) {
+
+   }
+}
+
 
 //add listener to add keyword button
 document.querySelector('.add-keyword').addEventListener('click', () => {
-   var keywordRow = document.querySelector('.keyword-row').innerHTML;
-   console.log(keywordRow);
+   var individualWords = document.querySelector('.individual-words').innerHTML;
+   var newRow = document.createElement('div');
+   newRow.innerHTML = individualWords;
+   newRow.classList.add('individual-words');
+   document.querySelector('.keyword-row').appendChild(newRow);
 })
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
-document.getElementById('save').addEventListener('click', saveOptions);
+document.getElementById('save').addEventListener('click', buildSaveArray);
