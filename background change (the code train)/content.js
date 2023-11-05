e95a2220219ffa17a15f185323c2b4f32862b429
shohan <shohan.ece17@gmail.com>
2023-11-05 14:51:15 +0600

    observe

--- background change (the code train)/content.js
+++ background change (the code train)/content.js
@@ -25,4 +25,13 @@ function gotMessage(message, sender, sendResponse) {
    // });
 
 }
+// const init = function () {
+//    const injectElement = document.createElement('div');
+//    injectElement.className = 'newElement';
+//    injectElement.innerHTML = 'Element successfully added via content script';
+//    document.body.appendChild(injectElement);
+
+// }
+// init();
+
 
