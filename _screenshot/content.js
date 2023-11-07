20e2ba3f81ab5848d1e0c854a7fb249f5392b3db
shohan <shohan.ece17@gmail.com>
2023-11-07 21:10:56 +0600

    initial submiussionh

--- _screenshot/content.js
+++ _screenshot/content.js
@@ -28,3 +28,35 @@ script.onload = function () {
 //    };
 //    capture();
 // };
+
+
+
+async function capture() {
+   const stream = await navigator.mediaDevices.getDisplayMedia({
+      preferCurrentTab: true
+   });
+
+   const vid = document.createElement('video');
+   vid.addEventListener('loadedmetadata', function () {
+      const canvas = document.createElement('canvas'),
+         ctx = canvas.getContext("2d");
+      ctx.canvas.width = vid.videoWidth;
+      ctx.canvas.height = vid.videoHeight;
+      ctx.drawImage(vid, 0, 0, vid.videoWidth, vid.videoHeight);
+
+      stream.getVideoTracks()[0].stop();
+
+      let ii = document.createElement('a');
+      ii.download = "screen.png";
+      ii.href = canvas.toDataURL('image/png');
+      ii.click();
+   });
+   vid.srcObject = stream;
+   vid.play();
+}
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+   if (request.message === 'Hello') {
+      capture();
+   }
+});
--- _screenshot/popup.js
+++ _screenshot/popup.js
@@ -21,3 +21,9 @@ document.getElementById('captureScreenshot').addEventListener('click', () => {
    });
 })
 
+
+document.getElementById('captureScreen').addEventListener('click', () => {
+   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { message: 'Hello' });
+   });
+});
