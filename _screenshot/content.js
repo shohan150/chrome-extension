9d99a0d4b54cfecd856deedf5a9ffc61747c389e
shohan <shohan.ece17@gmail.com>
2023-11-07 20:13:19 +0600

    html2canvas library failed to inject

--- _screenshot/content.js
+++ _screenshot/content.js
@@ -1,9 +1,8 @@
 const script = document.createElement('script');
-script.src = chrome.runtime.getURL('html2canvas.min.js');
+script.src = chrome.runtime.getURL('html2canvas.js');
 document.head.appendChild(script);
 script.onload = function () {
    function capture() {
-      console.log('check bro');
       html2canvas(document.body).then(canvas => {
          var aa = document.createElement("a");
          aa.download = "canvas screenshot.png";
@@ -13,3 +12,19 @@ script.onload = function () {
    };
    capture();
 };
+
+// const script2 = document.createElement('script');
+// script2.src = chrome.runtime.getURL('example.js');
+// document.head.appendChild(script2);
+// script2.onload = function () {
+//    test('check 2');
+//    function capture() {
+//       // html2canvas(document.body).then(canvas => {
+//       //    var aa = document.createElement("a");
+//       //    aa.download = "canvas screenshot.png";
+//       //    aa.href = canvas.toDataURL("image / png");
+//       //    aa.click();
+//       // })
+//    };
+//    capture();
+// };
---
+++ _screenshot/example.js
@@ -0,0 +1,5 @@
+console.log('made for test purpose');
+function test(x) {
+   console.log('made for test purpose 2 ' + x);
+}
+test('check 1');
