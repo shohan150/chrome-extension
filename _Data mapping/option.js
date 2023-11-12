5c07fe19b2756b0256a4f1f911d01cc47ff6a69a
shohan <shohan.ece17@gmail.com>
2023-11-12 18:59:18 +0600

    restart

--- _Data mapping/option.js
+++ _Data mapping/option.js
@@ -1,19 +1,32 @@
 var newField = document.getElementById('addField');
 var saveField = document.getElementById('saveField');
+var dltField = document.getElementById('dltField');
 var container = document.querySelector('.container');
 
 
-var fieldHTML = `<div class="fields">
-<label>Name : </label>
-<input type="text" id="fieldName">
-<label>Path : </label>
-<input type="text" id="fieldPath">
-</div>`
 newField.addEventListener('click', () => {
    container.innerHTML += fieldHTML;
 });
 
 saveField.addEventListener('click', () => {
+   saveTheData();
+});
+
+dltField.addEventListener('click', () => {
+   chrome.storage.local.remove('fieldsData');
+   showStoredData();
+})
+
+
+var fieldHTML =
+   `<div class="fields">
+      <label>Name : </label>
+      <input type="text" id="fieldName">
+      <label>Path : </label>
+      <input type="text" id="fieldPath">
+   </div>`;
+
+function saveTheData() {
    var particularField = document.querySelectorAll('.container .fields');
    var fieldsData = [];
    particularField.forEach(field => {
@@ -21,11 +34,25 @@ saveField.addEventListener('click', () => {
       const dataPath = field.querySelector('input[id="fieldPath"]').value;
       fieldsData.push({ data: dataName, path: dataPath });
    })
-
    chrome.storage.local.set({ fieldsData });
-   // chrome.storage.local.get(['fieldsData'], function (result) {
-   //    result.fieldsData.forEach(val => {
-   //       console.log(val.data, val.path);
-   //    })
-   // });
-});
+}
+
+function showStoredData() {
+   chrome.storage.local.get(['fieldsData'], function (result) {
+      if (!result.fieldsData) {
+         container.innerHTML = '';
+      } else {
+         result.fieldsData.forEach(val => {
+            var showStoredValues =
+               `<div class="fields">
+               <label>Name : </label>
+               <input type="text" id="fieldName" value="${val.data}">
+               <label>Path : </label>
+               <input type="text" id="fieldPath" value="${val.path}">
+            </div>`
+            container.innerHTML += showStoredValues;
+         });
+      }
+   });
+}
+showStoredData();
