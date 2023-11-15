371411ca5c30c7c6a68818cf0ac4be6c5611888e
shohan <shohan.ece17@gmail.com>
2023-11-15 22:52:04 +0600

    tasks to dooo. nije e halka dos kheyechi. r seta nijer dosh ei. tobe alhamdulillah. situation tackled down.

--- _Data mapping/content.js
+++ _Data mapping/content.js
@@ -13,6 +13,58 @@ chrome.storage.local.get(['fieldsData'], function (result) {
 });
 
 
+// gives the whole path to that element
+// document.addEventListener('click', function (event) {
+//    var selector = getSelector(event.target);
+//    console.log('Element selector:', selector);
+// });
+
+// function getSelector(element) {
+//    if (!element) return;
+
+//    var path = [];
+//    while (element.parentNode) {
+//       var index = [].indexOf.call(element.parentNode.children, element) + 1;
+//       var tagName = element.tagName.toLowerCase();
+//       var identifier = tagName;
+//       if (element.id) {
+//          identifier += '#' + element.id;
+//       } else if (element.className) {
+//          identifier += '.' + element.className.trim().replace(/\s+/g, '.');
+//       }
+//       path.unshift(identifier + ':nth-child(' + index + ')');
+//       element = element.parentNode;
+//    }
+
+//    return path.join(' > ');
+// }
+
+
+function getSelector(element) {
+   if (!element) return;
+
+   var selector = element.tagName.toLowerCase();
+   if (element.id) {
+      selector += '#' + element.id;
+   } else if (element.className) {
+      selector += '.' + element.className.replace(/\s+/g, '.');
+   }
+
+   return selector;
+}
+document.addEventListener('click', function (event) {
+   var selector = getSelector(event.target);
+   console.log('Element selector:', selector);
+});
+
+
+//identify all messages
+//from content.js to popup.js
+//popup.js to popup.html
+//adjust according to query selector
+//add show data button
+
+
 
 
 
